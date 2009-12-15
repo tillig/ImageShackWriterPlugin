ea93851963c47704312ced059ad69c779a048876
travis.illig <travis.illig@eb3a408c-e9bb-11de-9229-05bbbb8527c3>
2009-12-15 21:03:55 +0000

    Created tag for release 1.0.0.0.

---
+++ tags/release/1.0.0.0/AssemblyVersion.cs
@@ -0,0 +1,6 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyProduct("ImageShack Upload Plugin")]
+[assembly: AssemblyCopyright("Copyright © 2009 ImageShackWriterPlugin Contributors")]
+[assembly: AssemblyVersion("1.0.0.0")]
